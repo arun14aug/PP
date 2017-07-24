ba7532eca322bcfc63e822d7c0416a4e629f82f9
arun14aug <arun.sharma@sourcefuse.com>
2017-07-24 18:48:55 +0530

    First Update
    First Update

---
+++ app/src/main/java/com/pinlesspay/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.pinlesspay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+ * Created by arun.sharma on 7/17/2017.
+ */
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
